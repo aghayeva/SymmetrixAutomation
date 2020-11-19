4fd40d698a2b62eaa613b368d50ebf82e790fcd0
aghayeva <sadaaghayeva@gmail.com>
2020-11-18 23:12:26 -0600

    commit

---
+++ src/test/java/io/symmetrix/utilities/ConfigurationReader.java
@@ -0,0 +1,42 @@
+package io.symmetrix.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    /**
+     * 1. We create Properties Object
+     *  - Inside Static Block
+     * 2. Create FileInputStream object
+     * 3. Get the path of Configuration.properties and pass into FileInputStream object parameter and open the file
+     * 4. Load the file into properties object
+     * 5. close the file
+     *  -
+     * 6. Create public static getProperty() method that returns String and accepts String parameter
+     */
+
+        private static Properties properties=new Properties();
+
+        static {
+
+            try {
+                FileInputStream file=new FileInputStream("Configuration.properties");
+                properties.load(file);
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Properties file not found.");
+            }
+
+        }
+
+    /**
+     * This method returns property value from configuration.properties file
+     * @param keyName property name
+     * @return property value
+     */
+        public static String getProperty(String keyName) {
+            return properties.getProperty(keyName);
+        }
+}
